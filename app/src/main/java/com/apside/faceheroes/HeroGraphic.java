dcf498d81d072eb571cfca1319bbb62532931874
Christophe Jollivet <christophe.jollivet@gmail.com>
2017-08-04 15:32:24 +0200

    first state

---
+++ app/src/main/java/com/apside/faceheroes/HeroGraphic.java
@@ -0,0 +1,61 @@
+package com.apside.faceheroes;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import com.apside.faceheroes.external.GraphicOverlay;
+
+
+
+class HeroGraphic extends GraphicOverlay.Graphic {
+
+    private volatile PointF mLeftPosition;
+    private volatile PointF mRightPosition;
+
+    private Paint mMaskColor;
+
+    private float MASK_SIZE = 30f;
+    private float MASK_WIDTH_FACTOR = 0.25f;
+
+
+    HeroGraphic(GraphicOverlay overlay) {
+        super(overlay);
+        mMaskColor = new Paint();
+        mMaskColor.setColor(Color.BLACK);
+        mMaskColor.setStyle(Paint.Style.STROKE);
+        mMaskColor.setStrokeWidth(MASK_SIZE);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        PointF detectLeftPosition = mLeftPosition;
+        PointF detectRightPosition = mRightPosition;
+        if ((detectLeftPosition == null) || (detectRightPosition == null)) {
+            return;
+        }
+
+        PointF leftPosition =
+                new PointF(translateX(detectLeftPosition.x), translateY(detectLeftPosition.y));
+        PointF rightPosition =
+                new PointF(translateX(detectRightPosition.x), translateY(detectRightPosition.y));
+
+        // Use the inter-eye distance to set the size of the eyes.
+        float distance = (float) Math.sqrt(
+                Math.pow(rightPosition.x - leftPosition.x, 2) +
+                        Math.pow(rightPosition.y - leftPosition.y, 2));
+        float radius = MASK_WIDTH_FACTOR * distance;
+
+        canvas.drawCircle(leftPosition.x, leftPosition.y, radius, mMaskColor);
+        canvas.drawCircle(rightPosition.x, rightPosition.y, radius, mMaskColor);
+    }
+
+    void updateEyes(PointF leftPosition, PointF rightPosition) {
+        mLeftPosition = leftPosition;
+
+        mRightPosition = rightPosition;
+
+        postInvalidate();
+    }
+}
--- app/src/main/java/com/apside/faceheroes/MainActivity.java
+++ app/src/main/java/com/apside/faceheroes/MainActivity.java
@@ -1,13 +1,162 @@
 package com.apside.faceheroes;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.apside.faceheroes.external.CameraSourcePreview;
+import com.apside.faceheroes.external.GraphicOverlay;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiActivity;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="FaceTracker";
+
+    private CameraSource mCameraSource=null;
+    private CameraSourcePreview mPreview;
+    private GraphicOverlay mGraphicOverlay;
+
+    private static final int RC_HANDLE_GMS = 9001;
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
+        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        }else{
+            requestCameraPermission();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCameraSource();
+    }
+
+    private void createCameraSource() {
+        Context context = getApplicationContext();
+        FaceDetector detector = createFaceDetector(context);
+
+        mCameraSource = new CameraSource.Builder(context, detector)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedPreviewSize(320, 240)
+                .setRequestedFps(60.0f)
+                .setAutoFocusEnabled(true)
+                .build();
+
+    }
+
+    private FaceDetector createFaceDetector(Context context) {
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .setTrackingEnabled(true)
+                .setMode(FaceDetector.FAST_MODE)
+                .setProminentFaceOnly(false)
+                .setMinFaceSize(0.15f)
+                .build();
+        Detector.Processor<Face> processor;
+        MultiProcessor.Factory<Face> factory = new MultiProcessor.Factory<Face>(){
+            @Override
+            public Tracker<Face> create (Face face){
+                return new HeroFacetracker(mGraphicOverlay);
+            }
+        };
+        processor = new MultiProcessor.Builder<>(factory).build();
+
+        detector.setProcessor(processor);
+
+        if (!detector.isOperational()) {
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+            IntentFilter lowStorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowStorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, "Face detector cannot be downloaded due to ...", Toast.LENGTH_LONG).show();
+                Log.w(TAG, "Face detector depedencies cannot be downloaded due to ...");
+            }
+        }
+        return detector;
+    }
+
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted, Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener =new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions, RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        Snackbar.make(mGraphicOverlay, "Access to the camera is needed for detection",
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", listener)
+                .show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mCameraSource != null){
+            mCameraSource.release();
+        }
+    }
+
+    private void startCameraSource() {
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dlg = GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dlg.show();
+        }
+
+        if (mCameraSource != null){
+            try{
+                mPreview.start(mCameraSource, mGraphicOverlay);
+            }catch(IOException e){
+                Log.e(TAG, "Unable to start camera source.",e);
+                mCameraSource.release();
+                mCameraSource = null;
+            }
+        }
     }
 }
